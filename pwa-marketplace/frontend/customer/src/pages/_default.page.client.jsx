6c92df99762dd9d035925a66165ba4dd8273f3f2
gretamal26377 <gretamal263@hotmail.com>
2025-12-30 20:08:02 -0300

    271th Commit: Modifying Project Customer Frontend to work correctly with vite-plugin-ssr

--- pwa-marketplace/frontend/customer/src/pages/_default.page.client.jsx
+++ pwa-marketplace/frontend/customer/src/pages/_default.page.client.jsx
@@ -8,7 +8,9 @@ import "../../../globals.css";
 
 export { render };
 
-function render(pageContext) {
+// pageContext: Parameter passed by vite-plugin-ssr. Purpose: To pass information about the page being rendered,
+// such as the URL, route parameters, and any other data needed for client-side rendering 
+function render(pageContext) {  // eslint-disable-line no-unused-vars
   hydrateRoot(
     document.getElementById("root"),
     <React.StrictMode>
